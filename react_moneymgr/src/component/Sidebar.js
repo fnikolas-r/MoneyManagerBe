a32df3cf302657a57770a67a8ea7eae48e47d590
fnikolas-r <nikolasfernando2001@gmail.com>
2023-02-13 20:29:12 +0800

    Create essential UI

--- react_moneymgr/src/component/Sidebar.js
+++
@@ -1,9 +0,0 @@
-function Sidebar() {
-    return <aside className="w-64" aria-label="Sidebar">
-        <div className="overflow-y-auto py-4 px-3 bg-gray-50 rouded dark:bg-gray-800">
-            Hello World
-        </div>
-    </aside>
-}
-
-export default Sidebar;
